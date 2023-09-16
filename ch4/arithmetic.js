d0d0e427c17a87d77d7e7a9307b80825e080a04d
kyoungjuu <143710732+kyoungjuu@users.noreply.github.com>
2023-09-16 22:57:30 +0900

    Create arithmetic.js

---
+++ ch4/arithmetic.js
@@ -0,0 +1,56 @@
+var a = 3;
+var b = 2;
+
+console.log('더하기 결과 : ' + (a + b));
+console.log('뺴기 결과 : ' + (a - b));
+console.log('곱하기 결과 : ' + (a * b));
+console.log('나누기 / 결과 : ' + (a / b));
+console.log('나누기 % 결과 : ' + (a % b));
+console.log('거듭제곱 결과 : ' + (a ** b));
+
+console.log('hello' + 'world');
+
+console.log('긴급출동' + 119);//문자
+console.log('1' + 1);//문자
+
+console.log(+"1024"+1);//숫자
+console.log(+true);//숫자
+console.log(+false);//숫자
+
+console.log('10' -2)  //숫자
+console.log('10' *2)  //숫자
+console.log('10' /2)  //숫자
+console.log('10' -'2')//숫자
+console.log('10' *'2')//숫자
+console.log('10' /'2')//숫자
+
+
+var a = 3;
+var b = a+2;
+var c =b-a;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = 3;
+a = a + 2;
+console.log(a);
+a = a - 2;
+console.log(a);
+a = a * 5;
+console.log(a);
+a = a / 5;
+console.log(a);
+
+var a = 3;
+a += 2;
+console.log(a);
+a -= 2;
+console.log(a);
+a *= 5;
+console.log(a);
+a /= 5;
+console.log(a);
+
+
