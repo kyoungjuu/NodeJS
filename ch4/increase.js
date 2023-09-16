355ea94c4227b129a45c365b1440dd06a207b072
kyoungjuu <143710732+kyoungjuu@users.noreply.github.com>
2023-09-16 22:57:18 +0900

    Create increase.js

---
+++ ch4/increase.js
@@ -0,0 +1,10 @@
+++a //a 값을 먼저 1 증가시킨 후 연산을 진행
+a++ //연산을 진행한후 a 값을 1 증가
+
+var a =3;
+console.log(a); //3
+console.log(++a); //4
+console.log(a++); //4
+console.log(a); //5
+
+//상수에는 증감연산자 적용되지 않는다.
