c29ebcf4af3e4e4387378ec887ef11a2b6348d79
kyoungjuu <143710732+kyoungjuu@users.noreply.github.com>
2023-09-19 23:56:01 +0900

    Create condition.js

---
+++ ch5/condition.js
@@ -0,0 +1,28 @@
+if(true)
+{
+    console.log('무조건 참입니다.');
+}
+
+var num = 20;
+if(num>10)
+{
+    console.log('입력값은 10보다 큽니다.');
+}
+if(num<10)
+{
+    console.log('입력값은 10보다 작습니다.');
+}
+if(num==10)
+{
+    console.log('입력값은 10과 같습니다.');
+}
+
+var num = 5;
+if(num > 10)
+{
+    console.log('입력값은 10보다 큽니다.');
+}
+else
+{
+    console.log('입력값은 크지 않습니다.');
+}
