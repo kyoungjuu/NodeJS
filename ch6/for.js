e59cd738c19c7e10df13fea0d40d5183b75e2e70
kyoungjuu <143710732+kyoungjuu@users.noreply.github.com>
2023-09-24 23:20:25 +0900

    Create for.js

---
+++ ch6/for.js
@@ -0,0 +1,31 @@
+for(var i = 0; i <5; i++)
+{
+    console.log('javascript');
+}
+
+for(var i = 0; i < 10; i++)
+{
+    console.log('*');
+}
+
+for(var i = 1; i < 13; i++)
+{
+    console.log(i + '월');
+}
+
+for(var i = 2; i <= 100; i+=2)
+{
+    console.log(i);
+}
+
+var sum = 0;
+for(var i = 1; i < 11; i++)
+{
+    sum = sum + i;
+}
+console.log(sum);
+
+for(var i = 1; i < 10; i++)
+{
+    console.log('2*'+ i + '=' + 2*i)
+}
