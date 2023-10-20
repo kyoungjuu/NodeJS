ec85815fc8d0bc0cfe00b74d91d497d9f1ee74bf
서경주 <rudwnvheh@naver.com>
2023-10-21 00:19:09 +0900

    Create arrow.js

---
+++ ch9/arrow.js
@@ -0,0 +1,11 @@
+let plus = (a,b) => a+b;
+let result = plus(10, 20);
+console.log('두 수의 합 : '+ result);
+
+let plus = a => a+1;
+let result = plus(10);
+console.log('값의 1 증가 : '+ result);
+
+let plus = () => '플러스';
+let result = plus();
+console.log('A'+ result);
