6aaee836a76460d8bc3615e88861968884ba5576
서경주 <rudwnvheh@naver.com>
2023-10-21 00:19:24 +0900

    Create tmpstring.js

---
+++ ch9/tmpstring.js
@@ -0,0 +1,13 @@
+const name = '이성현';
+const age = 25;
+const height = 178.5;
+
+console.log("My name is" + name + "," + "My age is" + age + "," + "My height is" + height);
+
+console.log(`My name is ${name}, My age is ${age}, My height is ${height}`);
+
+console.log("My name is" + name + "\n" + "My age is" + age + "\n" + "My height is" + height);
+
+console.log(`My name is ${name}
+My age is ${age}
+My height is ${height}`);
