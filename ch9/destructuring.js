5d24d71b3eb9c94c7b92bc8b0fdf3237a47d4257
서경주 <rudwnvheh@naver.com>
2023-10-21 00:19:12 +0900

    Create destructuring.js

---
+++ ch9/destructuring.js
@@ -0,0 +1,11 @@
+
+const [b,g,r = 'blue'] = ['red', 'green'];
+console.log(r);
+console.log(g);
+console.log(b);
+
+let{id, pw, name, age} = {id : 'jamsuham', pw : '1234', name : '잠수함', age : 30,};
+console.log(id);
+console.log(pw);
+console.log(name);
+console.log(age);
